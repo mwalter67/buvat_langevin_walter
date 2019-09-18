7a1872f069efbbe284c0cab4f18cbf380451d6a9
mwalter67 <54897895+mwalter67@users.noreply.github.com>
2019-09-18 17:47:05 +0200

    Modification Mickaël
    -Modification de la ShinyApp
    - Rajout du dossier www pour le CSS et image

--- server.R
+++ server.R
@@ -1,27 +1,65 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(data.table)
 library(shiny)
+library(ggplot2)
 library(e1071)
+
+data=fread("C:/Users/mikew/OneDrive/Documents/MASTER 2 ESA/S1/SVM/Projet SVM/creditcard.csv",sep=',')
+attach(data)
+set.seed(1)
+essai=sample(1:nrow(data),10000)
+data_essai=data[essai,]
+class_essai=Class[essai]
+
+choix_svm=function(kernel, cout, deg){
+    set.seed(5)
+    svm_essai=svm(Class~. , data=data_essai, kernel=kernel, type="C-classification", cost=cout, degree=deg )
+    pred_essai=predict(svm_essai, data_essai)
+    tab=table(pred_essai,class_essai)
+    err=mean(data_essai$Class!=pred_essai)*100
+    print(svm_essai)   
+    print(tab)
+    
+}
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    
+    output$text1 <- renderText("Essai du texte 1")
+    
+    output$Plot <- renderPlot({
+        
+        
+        
+        
+        # Construct sample data set - completely separated
+        set.seed(10)
+        x <- matrix(rnorm(20*2), ncol = 2)
+        y <- c(rep(-1,10), rep(1,10))
+        x[y==1,] <- x[y==1,] + 3/2
+        dat <- data.frame(x=x, y=as.factor(y))
+        
+        # Plot data
+        ggplot(data = dat, aes(x = x.2, y = x.1, color = y, shape = y)) + 
+            geom_point(size = 2) +
+            scale_color_manual(values=c("#000000", "#FF0000")) +
+            theme(legend.position = "none") +
+            ggtitle("Exemple d'un SVM avec un échantillon séparable")+
+            theme(plot.title = element_text(hjust = 0.5))+
+            labs(x="X")+
+            labs(y="Y")
+        
+        
     })
+    
+    output$text2 <- renderText("Essai du texte 2")
+    
+    output$table1 <- renderPrint({
+        choix_svm(input$kernel,input$cout, input$deg)
+        })
+    
+    
+
+    
 
 })
--- ui.R
+++ ui.R
@@ -1,34 +1,89 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(ggplot2)
 library(e1071)
+library(data.table)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+shinyUI(fluidPage(title="Projet SVM",
+                  tags$head(includeCSS("C:/Users/mikew/OneDrive/Documents/MASTER 2 ESA/S1/SVM/Projet SVM/TEST_BIS/www/app.css")),
+                  tags$img(style="position: absolute; bottom: 0; right: 0; border: 0;width: 150px; height: 150px",
+                           src="logoESA.png"
+                           ),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+    div(id = "header",
+        div(id = "title",
+            "Implémentation d'un SVM dans le cadre d'un risque à la fraude"
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+        div(id = "subtitle",
+            "Démonstrateur crée par",
+            tags$a(href = "mailto:mailto:antoinebuvat74@gmail.com", "BUVAT Antoine"),",",
+            tags$a(href = "mailto:mailto:langevin.julien@gmail.com", "LANGEVIN Julien"), "et ",
+            tags$a(href = "mailto:mailto:mikewalter@hotmail.fr", "WALTER Mickaël")
         )
-    )
+    ),
+    
+   
+        navlistPanel(
+            tabPanel(
+                title="Comment utiliser ce démonstrateur",
+                mainPanel(
+                    textOutput("text1")
+                )
+            ),
+            tabPanel(
+                title="Qu'est ce qu'un SVM?",
+                
+                mainPanel(
+                    plotOutput("Plot")
+                )
+            ),
+            
+            tabPanel(
+                title="Présentation de nos données",
+                mainPanel(
+                    textOutput("text2")
+                )
+            ),
+            tabPanel(
+                title="Intéraction intéractive avec le SVM",
+                sidebarLayout(
+                    sidebarPanel(
+                        selectInput("kernel",
+                                    "Choix du noyau",
+                                    choices=c("linear","polynomial","radial","sigmoid"),
+                                    multiple=FALSE,
+                                    selected="linear"
+                        ),
+                        conditionalPanel("input.kernel =='polynomial'",
+                                         sliderInput("deg",
+                                                     "Choix du degré du polynôme",
+                                                     min=3,
+                                                     max=10,
+                                                     value=3,
+                                                     step=1
+                                                     
+                                         )
+                                         
+                        )
+                        
+                    ,
+                        selectInput("cout",
+                                    "Coût de pénalisation",
+                                    choices=c(1,3,5,10),
+                                    multiple=FALSE,
+                                    selected=1
+                        )
+                    ),
+                        
+                        mainPanel(
+                            verbatimTextOutput("table1")
+                            
+                        )
+                    )
+                    
+                    
+                
+            )
+        )
+
 ))
