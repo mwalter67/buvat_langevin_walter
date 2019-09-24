4a959f8ddcb2ee3d201deb14b99ed9216c285ff8
AntoineBuvat <54897754+AntoineBuvat@users.noreply.github.com>
2019-09-24 21:15:43 +0200

    chemin
    rajout chemin antoine (ui+server)

--- server.R
+++ server.R
@@ -5,120 +5,123 @@ library(data.table)
 library(pROC)
 library(rmarkdown)
 
-resample=fread("C:/Users/mikew/OneDrive/Documents/GitHub/buvat_langevin_walter/creditcard_rus.csv",sep=',')
-test=fread("C:/Users/mikew/OneDrive/Documents/GitHub/buvat_langevin_walter/creditcard_test.csv",sep=',')
+#resample=fread("C:/Users/mikew/OneDrive/Documents/GitHub/buvat_langevin_walter/creditcard_rus.csv",sep=',')
+#test=fread("C:/Users/mikew/OneDrive/Documents/GitHub/buvat_langevin_walter/creditcard_test.csv",sep=',')
+resample=fread("C:/Users/util/Documents/GitHub/buvat_langevin_walter/creditcard_rus.csv",sep=',')
+test=fread("C:/Users/util/Documents/GitHub/buvat_langevin_walter/creditcard_test.csv",sep=',')
+
 attach(resample)
 names(resample)
 
 choix_svm=function(kernel, cout, deg){
-    set.seed(5)
-    svm_resample=svm(Class~. , data=resample, kernel=kernel, type="C-classification", cost=cout, degree=deg )
-    pred_essai=predict(svm_resample, resample)
-    tab=table(pred_essai,Class)
-    err=mean(resample$Class!=pred_essai)*100
-    print(svm_resample)   
+  set.seed(5)
+  svm_resample=svm(Class~. , data=resample, kernel=kernel, type="C-classification", cost=cout, degree=deg )
+  pred_essai=predict(svm_resample, resample)
+  tab=table(pred_essai,Class)
+  err=mean(resample$Class!=pred_essai)*100
+  print(svm_resample)   
 }
 
 confusion=function(kernel, cout, deg){
-    set.seed(5)
-    svm_resample=svm(Class~. , data=resample, kernel=kernel, type="C-classification", cost=cout, degree=deg )
-    pred_essai=predict(svm_resample, resample)
-    tab=table(pred_essai,Class)
-    err=mean(resample$Class!=pred_essai)*100
-    tab=data.frame(tab)
-    confusion=ggplot(data =  tab, mapping = aes(x = pred_essai, y = Class)) + 
-        geom_tile(aes(fill = Freq), colour = "black") +
-        geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
-        scale_fill_gradient(low = "white", high = "white") +
-        theme_bw() + theme(legend.position = "none") +
-        ggtitle("Matrice de confusion de l'échantillon d'apprentissage") +
-        xlab("Classe prédite") + ylab("Classe observée") +
-        theme(plot.title = element_text(hjust = 0.5))
-    print(confusion)
+  set.seed(5)
+  svm_resample=svm(Class~. , data=resample, kernel=kernel, type="C-classification", cost=cout, degree=deg )
+  pred_essai=predict(svm_resample, resample)
+  tab=table(pred_essai,Class)
+  err=mean(resample$Class!=pred_essai)*100
+  tab=data.frame(tab)
+  confusion=ggplot(data =  tab, mapping = aes(x = pred_essai, y = Class)) + 
+    geom_tile(aes(fill = Freq), colour = "black") +
+    geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+    scale_fill_gradient(low = "white", high = "white") +
+    theme_bw() + theme(legend.position = "none") +
+    ggtitle("Matrice de confusion de l'échantillon d'apprentissage") +
+    xlab("Classe prédite") + ylab("Classe observée") +
+    theme(plot.title = element_text(hjust = 0.5))
+  print(confusion)
 }
 
 confusion2=function(kernel, cout, deg){
-    set.seed(5)
-    svm_resample=svm(Class~. , data=resample, kernel=kernel, type="C-classification", cost=cout, degree=deg )
-    pred2=predict(svm_resample,data=resample,newdata = test, probability = FALSE)
-    summary(pred2)
-    
-    tab2=table(pred2,test$Class)
-    tab2=as.data.frame(tab2)
-    err2=mean(test$Class!=pred2)*100
-    err2
-    
-    confusion=ggplot(data =  tab2, mapping = aes(x = pred2, y = Var2)) + 
-        geom_tile(aes(fill = Freq), colour = "black") +
-        geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
-        scale_fill_gradient(low = "white", high = "white") +
-        theme_bw() + theme(legend.position = "none") +
-        ggtitle("Matrice de confusion de l'échantillon test") +
-        xlab("Classe prédite") + ylab("Classe observée") +
-        theme(plot.title = element_text(hjust = 0.5))
-    print(confusion)
-    
+  set.seed(5)
+  svm_resample=svm(Class~. , data=resample, kernel=kernel, type="C-classification", cost=cout, degree=deg )
+  pred2=predict(svm_resample,data=resample,newdata = test, probability = FALSE)
+  summary(pred2)
+  
+  tab2=table(pred2,test$Class)
+  tab2=as.data.frame(tab2)
+  err2=mean(test$Class!=pred2)*100
+  err2
+  
+  confusion=ggplot(data =  tab2, mapping = aes(x = pred2, y = Var2)) + 
+    geom_tile(aes(fill = Freq), colour = "black") +
+    geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+    scale_fill_gradient(low = "white", high = "white") +
+    theme_bw() + theme(legend.position = "none") +
+    ggtitle("Matrice de confusion de l'échantillon test") +
+    xlab("Classe prédite") + ylab("Classe observée") +
+    theme(plot.title = element_text(hjust = 0.5))
+  print(confusion)
+  
 }
 
 
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    
-    output$text1 <- renderText("Essai du texte 1")
-    
-    output$Plot <- renderPlot({
-        
-        
-        
-        
-        # Construct sample data set - completely separated
-        set.seed(10)
-        x <- matrix(rnorm(20*2), ncol = 2)
-        y <- c(rep(-1,10), rep(1,10))
-        x[y==1,] <- x[y==1,] + 3/2
-        dat <- data.frame(x=x, y=as.factor(y))
-        
-        # Plot data
-        ggplot(data = dat, aes(x = x.2, y = x.1, color = y, shape = y)) + 
-            geom_point(size = 2) +
-            scale_color_manual(values=c("#000000", "#FF0000")) +
-            theme(legend.position = "none") +
-            ggtitle("Exemple d'un SVM avec un Ã©chantillon sÃ©parable")+
-            theme(plot.title = element_text(hjust = 0.5))+
-            labs(x="X")+
-            labs(y="Y")
-        
-        
-    })
-    
-    
-    output$table1 <- renderPrint({
-        choix_svm(input$kernel,input$cout, input$deg)
-        })
-    
-    output$confusion <- renderPlot({
-        confusion(input$kernel,input$cout, input$deg)
-        })
-        
-    output$confusion2 <- renderPlot({
-            confusion2(input$kernel,input$cout, input$deg)
-    })
-    
-    output$roc <- renderPlot({
-        set.seed(5)
-        svm_resample=svm(Class~. , data=resample, kernel=input$kernel, type="C-classification", cost=input$cout, degree=input$deg )
-        pred2=predict(svm_resample,data=resample,newdata = test, probability = FALSE)
-        summary(pred2)
-        tab2=table(pred2,test$Class)
-        err2=mean(test$Class!=pred2)*100
-        prednum2=as.numeric(pred2)
-        roc2=roc(test$Class,prednum2)
-        plot.roc(roc2, print.auc=T, col="blue", main= "Courbe ROC sur l'échantillon de test")
-        
-    })
-    
   
+  output$text1 <- renderText("Essai du texte 1")
+  
+  output$Plot <- renderPlot({
     
-
+    
+    
+    
+    # Construct sample data set - completely separated
+    set.seed(10)
+    x <- matrix(rnorm(20*2), ncol = 2)
+    y <- c(rep(-1,10), rep(1,10))
+    x[y==1,] <- x[y==1,] + 3/2
+    dat <- data.frame(x=x, y=as.factor(y))
+    
+    # Plot data
+    ggplot(data = dat, aes(x = x.2, y = x.1, color = y, shape = y)) + 
+      geom_point(size = 2) +
+      scale_color_manual(values=c("#000000", "#FF0000")) +
+      theme(legend.position = "none") +
+      ggtitle("Exemple d'un SVM avec un Ã©chantillon sÃ©parable")+
+      theme(plot.title = element_text(hjust = 0.5))+
+      labs(x="X")+
+      labs(y="Y")
+    
+    
+  })
+  
+  
+  output$table1 <- renderPrint({
+    choix_svm(input$kernel,input$cout, input$deg)
+  })
+  
+  output$confusion <- renderPlot({
+    confusion(input$kernel,input$cout, input$deg)
+  })
+  
+  output$confusion2 <- renderPlot({
+    confusion2(input$kernel,input$cout, input$deg)
+  })
+  
+  output$roc <- renderPlot({
+    set.seed(5)
+    svm_resample=svm(Class~. , data=resample, kernel=input$kernel, type="C-classification", cost=input$cout, degree=input$deg )
+    pred2=predict(svm_resample,data=resample,newdata = test, probability = FALSE)
+    summary(pred2)
+    tab2=table(pred2,test$Class)
+    err2=mean(test$Class!=pred2)*100
+    prednum2=as.numeric(pred2)
+    roc2=roc(test$Class,prednum2)
+    plot.roc(roc2, print.auc=T, col="blue", main= "Courbe ROC sur l'échantillon de test")
+    
+  })
+  
+  
+  
+  
 })
--- ui.R
+++ ui.R
@@ -7,91 +7,93 @@ library(rmarkdown)
 
 
 shinyUI(fluidPage(title="Projet SVM",
-                  tags$head(includeCSS("C:/Users/mikew/OneDrive/Documents/MASTER 2 ESA/S1/SVM/Projet SVM/TEST_BIS/www/app.css")),
+                  #tags$head(includeCSS("C:/Users/mikew/OneDrive/Documents/MASTER 2 ESA/S1/SVM/Projet SVM/TEST_BIS/www/app.css")),
+                  #tags$head(includeCSS("C:/Users/util/Documents/GitHub/buvat_langevin_walter/www/app.css")),
                   tags$img(style="position: fixed; bottom: 0; left: 0; border: 0;width: 150px; height: 150px",
                            src="logoESA.png"
-                           ),
-
-    div(id = "header",
-        div(id = "title",
-            "Implémentation d'un SVM dans le cadre d'un risque à la fraude"
-        ),
-        div(id = "subtitle",
-            "Démonstrateur crée par",
-            tags$a(href = "mailto:mailto:antoinebuvat74@gmail.com", "BUVAT Antoine"),",",
-            tags$a(href = "mailto:mailto:langevin.julien@gmail.com", "LANGEVIN Julien"), "et ",
-            tags$a(href = "mailto:mailto:mikewalter@hotmail.fr", "WALTER Mickaël")
-        )
-    ),
-    
-   
-        navlistPanel(
-            tabPanel(
-                title="Comment utiliser ce démonstrateur",
-                mainPanel(
-                    
-                    textOutput("text1")
-                )
-            ),
-            tabPanel(
-                title="Qu'est ce qu'un SVM?",
-                
-                mainPanel(
-                    plotOutput("Plot")
-                )
-            ),
-            
-            tabPanel(
-                title="Présentation de nos données",
-                mainPanel(
-                    includeMarkdown("C:/Users/mikew/OneDrive/Documents/GitHub/buvat_langevin_walter/texte/pres_données.Rmd")
-                )
-            ),
-            tabPanel(
-                title="Intéraction intéractive avec le SVM",
-                sidebarLayout(
-                    sidebarPanel(
-                        selectInput("kernel",
-                                    "Choix du noyau",
-                                    choices=c("linear","polynomial","radial","sigmoid"),
-                                    multiple=FALSE,
-                                    selected="linear"
-                        ),
-                        conditionalPanel("input.kernel =='polynomial'",
-                                         sliderInput("deg",
-                                                     "Choix du degré du polynÃ´me",
-                                                     min=3,
-                                                     max=10,
-                                                     value=3,
-                                                     step=1
-                                                     
-                                         )
-                                         
-                        )
-                        
-                    ,
-                        selectInput("cout",
-                                    "Coût de pénalisation",
-                                    choices=c(1,3,5,10),
-                                    multiple=FALSE,
-                                    selected=1
-                        )
-                    ),
-                        
-                        mainPanel(
-                            verbatimTextOutput("table1"),
-                            plotOutput("confusion"),
-                            plotOutput("confusion2"),
-                            plotOutput("roc")
-                            
-                           
-                            
-                        )
-                    )
-                    
-                    
-                
-            )
-        )
-
+                  ),
+                  
+                  div(id = "header",
+                      div(id = "title",
+                          "Implémentation d'un SVM dans le cadre d'un risque à la fraude"
+                      ),
+                      div(id = "subtitle",
+                          "Démonstrateur crée par",
+                          tags$a(href = "mailto:mailto:antoinebuvat74@gmail.com", "BUVAT Antoine"),",",
+                          tags$a(href = "mailto:mailto:langevin.julien@gmail.com", "LANGEVIN Julien"), "et ",
+                          tags$a(href = "mailto:mailto:mikewalter@hotmail.fr", "WALTER Mickaël")
+                      )
+                  ),
+                  
+                  
+                  navlistPanel(
+                      tabPanel(
+                          title="Comment utiliser ce démonstrateur",
+                          mainPanel(
+                              
+                              textOutput("text1")
+                          )
+                      ),
+                      tabPanel(
+                          title="Qu'est ce qu'un SVM?",
+                          
+                          mainPanel(
+                              plotOutput("Plot")
+                          )
+                      ),
+                      
+                      tabPanel(
+                          title="Présentation de nos données",
+                          mainPanel(
+                              #includeMarkdown("C:/Users/mickew/OneDriveDocuments/GitHub/buvat_langevin_walter/texte/pres_données.Rmd")
+                              #includeMarkdown("C:/Users/util/Documents/GitHub/buvat_langevin_walter/texte/pres_données.Rmd")
+                          )
+                      ),
+                      tabPanel(
+                          title="Intéraction intéractive avec le SVM",
+                          sidebarLayout(
+                              sidebarPanel(
+                                  selectInput("kernel",
+                                              "Choix du noyau",
+                                              choices=c("linear","polynomial","radial","sigmoid"),
+                                              multiple=FALSE,
+                                              selected="linear"
+                                  ),
+                                  conditionalPanel("input.kernel =='polynomial'",
+                                                   sliderInput("deg",
+                                                               "Choix du degré du polynÃ´me",
+                                                               min=3,
+                                                               max=10,
+                                                               value=3,
+                                                               step=1
+                                                               
+                                                   )
+                                                   
+                                  )
+                                  
+                                  ,
+                                  selectInput("cout",
+                                              "Coût de pénalisation",
+                                              choices=c(1,3,5,10),
+                                              multiple=FALSE,
+                                              selected=1
+                                  )
+                              ),
+                              
+                              mainPanel(
+                                  verbatimTextOutput("table1"),
+                                  plotOutput("confusion"),
+                                  plotOutput("confusion2"),
+                                  plotOutput("roc")
+                                  
+                                  
+                                  
+                              )
+                          )
+                          
+                          
+                          
+                      )
+                  )
+                  
 ))
