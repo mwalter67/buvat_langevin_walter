deaa30c919604fdc63f71d574c5a2c54660ce6bf
mwalter67 <54897895+mwalter67@users.noreply.github.com>
2019-10-01 16:42:29 +0200

    correction png

--- server.R
+++ server.R
@@ -264,7 +264,7 @@ shinyServer(function(input, output) {
   
   output$image <- renderImage({
     # When input$n is 1, filename is ./images/image1.jpeg
-    filename <- file.path("www/explication_bdd.png")
+    filename <- file.path("www/explication_bdd.PNG")
     
     # Return a list containing the filename
     list(src = filename)
